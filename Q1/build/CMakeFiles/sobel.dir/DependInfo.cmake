15404a33ac1d25e10f594e6ab0402246a4e308b6
wine10001 <128204373+wine10001@users.noreply.github.com>
2023-04-04 17:16:35 +0800

    Add files via upload

---
+++ Q1/build/CMakeFiles/sobel.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/ee6470/docker-images/EE6470/hw2/Q1/Filter.cpp" "CMakeFiles/sobel.dir/Filter.cpp.o" "gcc" "CMakeFiles/sobel.dir/Filter.cpp.o.d"
+  "/home/user/ee6470/docker-images/EE6470/hw2/Q1/Initiator.cpp" "CMakeFiles/sobel.dir/Initiator.cpp.o" "gcc" "CMakeFiles/sobel.dir/Initiator.cpp.o.d"
+  "/home/user/ee6470/docker-images/EE6470/hw2/Q1/Testbench.cpp" "CMakeFiles/sobel.dir/Testbench.cpp.o" "gcc" "CMakeFiles/sobel.dir/Testbench.cpp.o.d"
+  "/home/user/ee6470/docker-images/EE6470/hw2/Q1/main.cpp" "CMakeFiles/sobel.dir/main.cpp.o" "gcc" "CMakeFiles/sobel.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
